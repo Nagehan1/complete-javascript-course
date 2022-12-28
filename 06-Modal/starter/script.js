8e098155f6b85562b97f5c2ac8cd51277edd0cc2
Nagehan Uyumaz <uyumaznagehan@gmail.com>
2022-12-28 15:40:50 +0000

    Working With Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,5 +2,22 @@
 const modal = document.querySelector('.modal');
 const overlay= document.querySelector('.overlay');
 const btnCloseModal= document.querySelector('.close-modal')
-const btnOpenModal=document.querySelector('.open-modal');
-console.log(btnOpenModal)
+const btnOpenModal=document.querySelectorAll('.show-modal');
+const openModal = function(){
+console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+}
+
+for(let i=0;i<btnOpenModal.length;i++)
+btnOpenModal[i].addEventListener('click',openModal)
+    
+    
+
+const closeModal=function(){
+     modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+btnCloseModal.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
+ 
