2cb4d0b2b52d8a05848923bbed7f9d06b91e40ee
Nagehan Uyumaz <uyumaznagehan@gmail.com>
2022-12-28 00:05:05 +0000

    [200~. Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,13 @@
-'use strict';
-console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent = 'Correct Number!')
-document.querySelector('.number').textContent=15;
-document.querySelector('.score').textContent=25
+// 'use strict';
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent = 'Correct Number!')
+// document.querySelector('.number').textContent=15;
+// document.querySelector('.score').textContent=25
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value=30;
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value=30;
+
+document.querySelector('.check').addEventListener('click',function(){
+    console.log(document.querySelector('.guess').value)
+})
